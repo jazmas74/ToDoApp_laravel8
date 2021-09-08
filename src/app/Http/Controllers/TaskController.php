5ef592a4566feb09023e53cfa61eb11d7e8cf80a
satoshi arai <jazmas74@gmail.com>
2021-09-08 13:23:56 +0900

    https://www.hypertextcandy.com/laravel-tutorial-todo-app-list-folders folder 表示まで

--- src/app/Http/Controllers/TaskController.php
+++ src/app/Http/Controllers/TaskController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Folder;
 use Illuminate\Http\Request;
 
 class TaskController extends Controller
@@ -13,7 +14,11 @@ class TaskController extends Controller
      */
     public function index()
     {
-        return "Hello world";
+        $folders = Folder::all();
+
+        return view('tasks/index', [
+            'folders' => $folders,
+        ]);
     }
 
     /**

dde5bb8917a74793b82381dd8d9e96b3f8accc19
satoshi arai <jazmas74@gmail.com>
2021-09-08 15:48:57 +0900

    https://www.hypertextcandy.com/laravel-tutorial-todo-app-list-folders 日付を返す処理まで

--- src/app/Models/Task.php
+++ src/app/Models/Task.php
@@ -2,10 +2,62 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Date;
 
 class Task extends Model
 {
     use HasFactory;
+
+    const STATUS = [
+        1 => [ 'label' => '未着手', 'class' => 'label-danger' ],
+        2 => [ 'label' => '着手中', 'class' => 'label-info' ],
+        3 => [ 'label' => '完了', 'class' => '' ],
+    ];
+
+    /**
+     * 状態のラベル
+     * @return string
+     */
+    public function getStatusLabelAttribute()
+    {
+        // 状態値
+        $status = $this->attributes['status'];
+
+        // 定義されていなければ空文字を返す
+        if (!isset(self::STATUS[$status])) {
+            return '';
+        }
+
+        return self::STATUS[$status]['label'];
+    }
+
+    /**
+     * 状態を表すHTMLクラス
+     * @return string
+     */
+    public function getStatusClassAttribute()
+    {
+        // 状態値
+        $status = $this->attributes['status'];
+
+        // 定義されていなければ空文字を返す
+        if (!isset(self::STATUS[$status])) {
+            return '';
+        }
+
+        return self::STATUS[$status]['class'];
+    }
+
+    /**
+     * 整形した期限日
+     * @return string
+     */
+    public function getFormattedDueDateAttribute()
+    {
+        return Carbon::parse($this->attributes['due_date'])->format('Y/m/d');
+    }
 }
+

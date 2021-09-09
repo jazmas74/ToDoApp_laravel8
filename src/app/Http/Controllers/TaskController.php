59e828d956cfb6b711296bdc81bfe6fd0c404242
satoshi arai <jazmas74@gmail.com>
2021-09-09 15:28:54 +0900

    https://www.hypertextcandy.com/laravel-tutorial-todo-app-list-folders タスクを保存する

--- src/app/Http/Controllers/TaskController.php
+++ src/app/Http/Controllers/TaskController.php
@@ -6,6 +6,7 @@ use App\Models\Folder;
 use App\Models\Task;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\CreateTask;
 
 class TaskController extends Controller
 {
@@ -34,68 +35,27 @@ class TaskController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
+     * GET /folders/{id}/tasks/create
      */
-    public function create()
+    public function showCreateForm(int $id)
     {
-        //
+        return view('tasks/create', [
+            'folder_id' => $id
+        ]);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
+    public function create(int $id,CreateTask $request)
     {
-        //
-    }
+        $current_folder = Folder::find($id);
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
+        $task = new Task();
+        $task->title = $request->title;
+        $task->due_date = $request->due_date;
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
+        $current_folder->tasks()->save($task);
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
+        return redirect()->route('tasks.index', [
+            'id' => $current_folder->id,
+        ]);
     }
 }
